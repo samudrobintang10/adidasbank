4191560c15608a6bae3b51004f2fbaa1f4307538
FaisalAuliaGhani <faisalauliaghani@gmail.com>
2025-03-01 17:31:50 +0700

    customer account

--- src/main/java/com/example/adidasbank/controller/AdidasBankController.java
+++ src/main/java/com/example/adidasbank/controller/AdidasBankController.java
@@ -1,4 +1,22 @@
 package com.example.adidasbank.controller;
 
+import com.example.adidasbank.entity.CustomerAccount;
+import com.example.adidasbank.service.CustomerAccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping
 public class AdidasBankController {
+
+    @Autowired
+    CustomerAccountService customerAccountService;
+
+    @GetMapping("/getBalance")
+    public ResponseEntity<CustomerAccount> getBalance(@RequestParam String request) {
+        CustomerAccount response = customerAccountService.getCustomerAccountByAccountNumber(request);
+
+        return ResponseEntity.ok(response);
+    }
 }
