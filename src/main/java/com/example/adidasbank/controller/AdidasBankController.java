a7f41da06b99ea03154918c052ccad8e5b11974f
HafizhDaffa <hafizhdaffa050901@gmail.com>
2025-03-01 17:49:04 +0700

    rate

--- src/main/java/com/example/adidasbank/controller/AdidasBankController.java
+++ src/main/java/com/example/adidasbank/controller/AdidasBankController.java
@@ -1,4 +1,59 @@
 package com.example.adidasbank.controller;
 
+import com.example.adidasbank.entity.ForeignExchangeMarket;
+import com.example.adidasbank.repository.ForeignExchangeMarketRepository;
+import com.example.adidasbank.service.ForeignExchangeMarketService;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
 public class AdidasBankController {
+
+    @Autowired
+    private ForeignExchangeMarketService foreignExchangeMarketService;
+
+    // Endpoint untuk menambahkan data baru
+    @PostMapping
+    public ResponseEntity<ForeignExchangeMarket> addExchangeMarket(@RequestBody ForeignExchangeMarket market) {
+        ForeignExchangeMarket createdMarket = foreignExchangeMarketService.addExchangeMarket(market);
+        return ResponseEntity.ok(createdMarket);
+    }
+
+    // Endpoint untuk mengambil semua data
+    @GetMapping
+    public ResponseEntity<List<ForeignExchangeMarket>> getAllExchangeMarkets() {
+        List<ForeignExchangeMarket> markets = foreignExchangeMarketService.getAllExchangeMarkets();
+        return ResponseEntity.ok(markets);
+    }
+
+    // Endpoint untuk mengambil data berdasarkan ID
+    @GetMapping("/getForeignExchangeMarket/{id}")
+    public ResponseEntity<ForeignExchangeMarket> getExchangeMarketById(@PathVariable Integer id) {
+        ForeignExchangeMarket market = foreignExchangeMarketService.getExchangeMarketById(id);
+        return ResponseEntity.ok(market);
+//        return market != null ? ResponseEntity.ok(market) : ResponseEntity.notFound().build();
+    }
+
+//    @GetMapping("/getForeignExchangeMarket/{currencyTo}")
+//    public ResponseEntity<ForeignExchangeMarket> getExchangePrice(@PathVariable String currencyTo) {
+//        ForeignExchangeMarket exchangePrice = foreignExchangeMarketService.getExchangePrice(currencyTo);
+//
+//        if (exchangePrice != null) {
+//            return ResponseEntity.ok(exchangePrice);
+//        } else {
+//            return ResponseEntity.notFound().build();
+//        }
+//    }
 }
+
+
+// Endpoint untuk menghapus data
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<Void> deleteExchangeMarket(@PathVariable int id) {
+//        foreignExchangeMarketService.deleteExchangeMarket(id);
+//        return ResponseEntity.noContent().build();
+//    }
--- src/main/java/com/example/adidasbank/entity/ForeignExchangeMarket.java
+++ src/main/java/com/example/adidasbank/entity/ForeignExchangeMarket.java
@@ -1,4 +1,29 @@
 package com.example.adidasbank.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "foreign_exchange_market")
 public class ForeignExchangeMarket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "currency_from")
+    private String currencyFrom;
+
+    @Column(name = "currency_to")
+    private String currencyTo;
+
+    @Column(name = "exchange_price")
+    private Float exchangePrice;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+//    @Column(name = "id_currency")
+//    private int idCurrency;
+
 }
